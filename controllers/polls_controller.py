35f91a6604d51a02885510229d055732c0a0edce
Dimonium-239 <dymitr.kuzmin@gmail.com>
2021-11-21 19:03:48 +0100

    Add endpoints for polls

---
+++ controllers/polls_controller.py
@@ -0,0 +1,22 @@
+from models.Polls import Polls
+from models.Questions import Questions
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
+
+
+def polls(request):
+    if request.method == 'GET':
+        polls_list = list(Polls.objects.values())
+        return JsonResponse(polls_list, safe=False)
+
+
+def polls_by_id(request, id_arg):
+    if request.method == 'GET':
+        poll = Polls.objects.get(id=id_arg)
+        return JsonResponse(model_to_dict(poll), safe=False)
+
+
+def questions_by_id(request, id_arg):
+    if request.method == 'GET':
+        questions = Questions.objects.get(poll_id=id_arg)
+        return JsonResponse(model_to_dict(questions), safe=False)
--- main/urls.py
+++ main/urls.py
@@ -17,8 +17,12 @@ from django.contrib import admin
 from django.urls import path
 
 import controllers.test_controller
+import controllers.polls_controller
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', controllers.test_controller.test_endpoint, name='test_endpoint')
+    path('test/', controllers.test_controller.test_endpoint, name='test_endpoint'),
+    path('polls/', controllers.polls_controller.polls, name='polls'),
+    path('polls/<int:id_arg>', controllers.polls_controller.polls_by_id, name='polls_by_id'),
+    path('polls/<int:id_arg>/questions', controllers.polls_controller.questions_by_id, name='questions_by_id')
 ]
